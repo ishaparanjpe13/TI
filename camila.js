1d43344c57fcf193e6fb626ee189b4f7eb3eacfa
ishaparanjpe13 <60287077+ishaparanjpe13@users.noreply.github.com>
2020-03-21 22:58:26 +0530

    FINAL

---
+++ camila.js
@@ -0,0 +1,37 @@
+let song;
+let ran;
+
+function preload () {
+  soundFormats ("mp3");
+  song = loadSound ("AllTheseYearslyricsCamilaCabelloRingtone.mp3");
+}
+
+function setup(){
+  createCanvas (400,400);
+  ran = false;
+
+}
+function draw () {
+  background (220);
+  if (ran==true)
+  {
+    let r= int(random(0,song.duration () -1));
+    song.jump(r,1);
+  }
+}
+function keyPressed () {
+  if (key=="a") {
+    song.play();
+  }
+  if (key=="k"){
+    song.stop ();
+  }
+  if (key=="r"){
+    song.reverseBuffer ();
+    song.play();
+  }
+}
+function mouseClicked (){
+  ran=!ran;
+}
+ 
